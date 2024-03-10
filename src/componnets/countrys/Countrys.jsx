c30a5851091a2ba7433fb6021d07b9318dc69acd
masud <masudranaacb@gmail.com>
2024-03-10 23:18:18 +0600

    added useState toggle

--- src/componnets/countrys/Countrys.jsx
+++ src/componnets/countrys/Countrys.jsx
@@ -1,21 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../county/Country";
+import './countrys.css'
 
 const Countrys = () => {
-    const [countrys,setCountrys]=useState([])
+    const [countrys, setCountrys] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch("https://restcountries.com/v3.1/all")
-        .then(res =>res.json())
-        .then(data=>setCountrys(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setCountrys(data))
+    }, [])
     return (
-        <div>
+        <div >
             <h3>Total Country:{countrys.length}</h3>
-            {
-                countrys.map(country=><Country key={name} country={country}></Country>)
-            }
+            <div className="countreis_container">
+                {
+                    countrys.map(country => <Country key={country.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/componnets/county/Country.jsx
+++ src/componnets/county/Country.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './country.css'
 const Country = ({country}) => {
-    console.log(country)
-    const {name}=country
+    // console.log(country)
+    const {name,flags,cca3}=country
+    const [visited,setVisited]=useState(false)
+    const handlerVisited=()=>{
+        setVisited(!visited)
+    }
+    console.log(visited)
     return (
         <div className="country_container">
+            <img src={flags.png} alt="" />
             <p>Corty Name:{name.common}</p>
+            <p>CCA3:{cca3}</p>
+            <button onClick={handlerVisited}>{visited?"visited":"Go Now"}</button>
         </div>
     );
 };
